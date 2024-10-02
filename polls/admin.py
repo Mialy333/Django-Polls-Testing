d86583606cd53de98d83c0a1910c65a4ccd7e835
Mialy Ratsimbazafy <mialy.ratsimbazafy@gmail.com>
2024-10-02 17:52:00 +0200

    changement des résultats en texte True et False par le décorateur display () + ajout de la barre latérale Filter

--- polls/admin.py
+++ polls/admin.py
@@ -5,9 +5,11 @@ from .models import Choice, Question
 """ Une question, 3 choix """
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    """ TabulaInline: pour optimiser l'affichage en tableau des objets liés sans prendre trop de place """
     model = Choice
     extra = 3
+    """ Penser à intégrer un test unitaire pour être sûre d'avoir uniquement 3 choix d'affiché """
 
 class QuestionAdmin(admin.ModelAdmin):
      fieldsets = [
@@ -15,6 +17,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
      inlines = [ChoiceInline]
+     list_display = ["question_text", "pub_date", "was_published_recently"]
+     list_filter=["pub_date"]
+     search_fields=["question_text"]
 
 
 admin.site.register(Question, QuestionAdmin)
