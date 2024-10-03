7ad572c25f25c2509232d39d36dab5399fa0843b
Mialy RATSIMBAZAFY <mialy.ratsimbazafy@gmail.com>
2024-10-03 08:13:06 +0200

    Fix module import

--- myPolls/wsgi.py
+++ myPolls/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Polls.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myPolls.settings')
 
 application = get_wsgi_application()
