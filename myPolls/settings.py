0d94590558724ca68079025b8e3bc54fd8b09e7d
Mialy RATSIMBAZAFY <mialy.ratsimbazafy@gmail.com>
2024-10-03 15:55:03 +0200

    Fix STATIC_ROOT and Whitenoise configuration

--- myPolls/settings.py
+++ myPolls/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-""" STATICFILES_DIRS = [
+
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # ou ajustez selon la structure de votre projet
-] """
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
