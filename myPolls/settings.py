10851dc7c49d26901e39ec3e0ace821fa4416efa
Mialy Ratsimbazafy <mialy.ratsimbazafy@gmail.com>
2024-10-03 07:10:49 +0200

    Correction des erreurs

--- myPolls/settings.py
+++ myPolls/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8$szvrseruii0woh=#35^k6-x9h(0f!_r0i)=l7jh-i(#22i=c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','127.0.0.0' ]
+ALLOWED_HOSTS = ['uvote.herokuapp.com', 'localhost']
 
 
 # Application definition
